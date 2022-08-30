06d6c692493c13ae337822ac39394e6c1d1735a0
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-08-31 01:17:46 +0300

    temp convert

---
+++ CW/0004- celsius to fahrenheit/index.js
@@ -0,0 +1,10 @@
+const convertTemp = (temp, type) => {
+  if (type == "celsius") {
+    return ((9 * temp + 32 * 5) / 5).toFixed(1) + " F";
+  }
+  if (type == "fahrenheit") {
+    return ((5 * (temp - 32)) / 9).toFixed(1) + " C";
+  }
+};
+
+console.log(convertTemp(50, "fahrenheit"));
