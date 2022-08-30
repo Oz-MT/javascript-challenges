7a2085c06d21f2a7c3ecccee051781469d3302c3
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-08-30 23:13:31 +0300

    triangle area

---
+++ CW/0003- triangle area/index.js
@@ -0,0 +1,7 @@
+//? Write a JavaScript code to find the area of a triangle.
+
+const triangleArea = (taban, yükseklik) => {
+  return (taban * yükseklik) / 2;
+};
+
+console.log(triangleArea(10, 5));
