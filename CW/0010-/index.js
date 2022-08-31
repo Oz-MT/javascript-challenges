3482959a78444cf515148c5cbce3ef2bc18798e3
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-08-31 13:25:10 +0300

    which is greatest of 3 numbers

---
+++ CW/0010-/index.js
@@ -0,0 +1,5 @@
+//? Write a JavaScript program that takes 3 integers from a user, multiplies all these numbers, and displays the sign of the product.
+//? Example1:
+//? number1 = 5, number2 = -4, number3 = 2 ⇒ product is -40. Then the sign is “-”
+//? Example2:
+//? number1 = -5, number2 = -4, number3 = 2 ⇒ product is 40. Then the sign is “+”
