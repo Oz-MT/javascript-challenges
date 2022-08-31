ea47e833cbe54e7329533ca4f1490206d94dbc27
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-08-31 13:33:24 +0300

    multiple and sign

--- CW/0010-/index.js
+++ CW/0010-/index.js
@@ -3,3 +3,17 @@
 //? number1 = 5, number2 = -4, number3 = 2 ⇒ product is -40. Then the sign is “-”
 //? Example2:
 //? number1 = -5, number2 = -4, number3 = 2 ⇒ product is 40. Then the sign is “+”
+
+const num1 = parseInt(prompt("Enter first number"));
+const num2 = parseInt(prompt("Enter second number"));
+const num3 = parseInt(prompt("Enter third number"));
+
+const multiple = num1 * num2 * num3;
+
+if (multiple < 0) {
+  console.log(`Product is ${multiple}. Then the sign is "-"`);
+} else if (multiple > 0) {
+  console.log(`Product is ${multiple}. Then the sign is "+"`);
+} else {
+  alert("Something went wrong");
+}
