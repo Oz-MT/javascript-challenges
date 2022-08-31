1aa002e185ee3115309beaaa741f2a3cd40e9cbb
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-08-31 13:05:16 +0300

    v1

--- CW/0008- which is greater/index.js
+++ CW/0008- which is greater/index.js
@@ -7,4 +7,6 @@ if (num1 > num2) {
   console.log(`${num1} is greater than ${num2}`);
 } else if (num2 > num1) {
   console.log(`${num2} is greater than ${num1}`);
+} else {
+  console.log("please enter a number");
 }
