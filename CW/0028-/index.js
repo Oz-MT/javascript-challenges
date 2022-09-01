048e5548a4c998551dc9697438538d6c2fe7715d
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-09-02 00:29:33 +0300

    palindrome

---
+++ CW/0028-/index.js
@@ -0,0 +1,13 @@
+//? Write a JavaScript function to convert a string into an abbreviated form.
+//? Example:
+//? func(“John Doe”)
+//? Output:
+//? “John D."      (Doe is abbreviated as D.)
+//? Example:
+//? func(“John”)
+//? Output:
+//? “John"        (Nothing to abbreviate)
+//? Example:
+//? func(“John Doe Marcus”)
+//? Output:
+//? “John D."   (Doe Marcus  is abbreviated as D.
