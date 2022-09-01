3e839ad32bc104af4de30b4921aed9b0492ce408
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-09-02 00:46:17 +0300

    abbreviated form

--- CW/0028- convert abbreviated form/index.js
+++ CW/0028- convert abbreviated form/index.js
@@ -11,3 +11,16 @@
 //? func(“John Doe Marcus”)
 //? Output:
 //? “John D."   (Doe Marcus  is abbreviated as D.
+
+const abbreviated = (str) => {
+  if (str.includes(" ")) {
+    const where = str.indexOf(" ");
+    const first = str.slice(0, where);
+    const second = str.slice(where + 1, where + 2) + ".";
+    return `${first} ${second}`;
+  } else {
+    return str;
+  }
+};
+
+console.log(abbreviated("John Doe Marcus"));
