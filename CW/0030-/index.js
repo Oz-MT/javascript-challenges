c27aad390607d1c9ef454d509be617da994eb439
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-09-02 23:56:50 +0300

    convert a string into a title format

---
+++ CW/0030-/index.js
@@ -0,0 +1,5 @@
+//? Write a JavaScript function to convert a string into a title format (The first letters of each word should be in capital letters)
+
+const titleForm = (str) => {
+  str.split();
+};
