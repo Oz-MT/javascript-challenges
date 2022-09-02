8d3fdebe65d4493893e5b251188993feeea8e69c
Beyza Öztekin <mattsoftwareng@gmail.com>
2022-09-03 00:18:28 +0300

    v1

--- CW/0030-/index.js
+++ CW/0030-/index.js
@@ -1,5 +1,11 @@
 //? Write a JavaScript function to convert a string into a title format (The first letters of each word should be in capital letters)
-
+let title = "";
 const titleForm = (str) => {
-  str.split();
+  const arr = str.split(" ");
+  for (let i = 0; i < arr.length; i++) {
+    title += arr[i][0].toUpperCase() + arr[i].slice(1) + " ";
+  }
+  return title;
 };
+
+console.log(titleForm("merhabalar ben mark"));
